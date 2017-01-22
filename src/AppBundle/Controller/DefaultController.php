398bd50304bc845bb5a45363080b22c7a882ce3f
Cédric GUICHERD <guicherd.cedric@outlook.com>
2017-01-22 16:03:48 +0100

    Ajout de la page subscribe

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,13 +3,33 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function homeAction(Request $request)
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homeAction()
     {
         return $this->render('AppBundle:default:index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction()
+    {
+        return $this->render('AppBundle:default:login.html.twig', [
+        ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function subscribeAction()
+    {
+        return $this->render('AppBundle:default:subscribe.html.twig', [
         ]);
     }
 }
--- src/AppBundle/Resources/public/js/init.js
+++ src/AppBundle/Resources/public/js/init.js
@@ -1,7 +1,16 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav();
+        $('.carousel.carousel-slider').carousel({
+            full_width: true,
+            indicators: true,
+            dist: 0,
+            setInterval: (function () {
+                $('.carousel').carousel('next');
+            }, 2000)
+        });
+        $('select').material_select();
 
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
